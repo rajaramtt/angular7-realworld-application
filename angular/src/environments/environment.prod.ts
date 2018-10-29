518a38f7d58f6f114fec4dfac11a2a466f9ddc1c
rajaramtavalam <rajaram.tavalam@gmail.com>
2018-10-30 01:04:51 +0530

    initial commit

---
+++ angular/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  host: 'http://localhost:3200',
+  login: '/login',
+  fotgotPassword: '/forgot-password',
+  resetPassword: '/reset-password',
+  countires: '/static/countries.json',
+  fileupload: '/fileupload',
+  deleteAttachment: '/filedelete',
+  clientSignup: '/client-signup'
+
+
+};
